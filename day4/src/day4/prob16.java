579b766fc6f4d2225e1520490fe870530c03d221
suusan11 <gawow11@gmail.com>
2018-07-10 20:24:44 -0700

    modified day4 prob16 file

--- day4/src/day4/prob16.java
+++ day4/src/day4/prob16.java
@@ -1,16 +1,18 @@
 package day4;
 
-//forの二重ループを用いて、以下のように九九の表を作りなさい。
-
-public class prob16 {
+public class Prob16 {
 
 	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
 		
-		for(int i = 1; i <= 9; i++) {
-            for(int j = 1; j <= 9; j++) {
-            System.out.print(i + "*" + j + "=" + i * j + " ");
-        }
-            System.out.println("");
-        }
+		for(int i = 1; i < 10; i++) {
+			for(int j = 1; j < 10; j++) {
+				System.out.print(i + "*" + j + "=" + i * j + " ");
+				if(j == 9) {
+					System.out.println(" ");
+				}
+			}
+		}
 	}
 }
